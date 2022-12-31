0759cd1caddee9ed835da6dbfd15405751fc6e28
quantium <alexcollsoutumuro@gmail.com>
2022-12-31 11:58:35 +0100

    add token sockets

--- v2/src/sockets/token.socket.ts
+++ v2/src/sockets/token.socket.ts
@@ -1,10 +1,24 @@
 import { Socket } from 'socket.io';
+import {
+  getTokenTransactions,
+  getTokenHolders
+} from '../controllers/token.controller';
 
+export const getTokenTransactionsSocket = async (socket: Socket): Promise<void> => {
+  socket.on('getTokenTransactions', async () => {
+    socket.emit('getTokenTransactionsRes', await getTokenTransactions());
+  });
+};
+
+export const getTokenHoldersSocket = async (socket: Socket): Promise<void> => {
+  socket.on('getTokenHolders', async () => {
+    socket.emit('getTokenHoldersRes', await getTokenHolders());
+  });
+};
 
 const tokenSocket = async (socket: Socket): Promise<void> => {
-
-  
-
+  await getTokenTransactionsSocket(socket);
+  await getTokenHoldersSocket(socket);
 };
 
 export default tokenSocket;
