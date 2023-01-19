edbe10ed3c92e3eb09e2b441106c8dd908d20cf2
quantium <alexcollsoutumuro@gmail.com>
2023-01-19 18:59:35 +0100

    let chatGPT solves the problem

--- v2/src/services/getUserNFTs.ts
+++ v2/src/services/getUserNFTs.ts
@@ -1,6 +1,7 @@
 import { Metaplex, keypairIdentity } from "@metaplex-foundation/js";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
-import fetch from "node-fetch";
+import * as http from "http";
+// import fetch from "node-fetch";
 
 const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL as string;
 const SOLANA_CONNECTION = new Connection(SOLANA_RPC_URL as string);
@@ -40,30 +41,43 @@ export const getUserNFTs = async (
   const NFTs: NFT[] = [];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   for (const rawNFT of rawNFTs) {
-    const metadata: any = await fetch(rawNFT.uri as string)
-      .then((response) => response.json())
-      .then((data) => data);
-    const mint = rawNFT.mintAddress.toBase58();
-    const supply = await _solanaConnection.getTokenSupply(new PublicKey(mint));
-    const nftRow = {
-      token: rawNFT.address.toBase58() as string,
-      mint: mint as string,
-      symbol: rawNFT.symbol as string,
-      supply: supply.value.uiAmount as number,
-      royalty: rawNFT.sellerFeeBasisPoints as number, // base 100 ex: 1000 = 10%
-      creators: rawNFT.creators.map((x: { address: string; share: number }) => {
-        return { pubkey: x.address, share: x.share };
-      }) as Creator[],
-      name: metadata.name as string,
-      description: metadata.description as string,
-      asset_uri: metadata.image as string,
-      metadata: metadata.attributes.map(
-        (x: { trait_type: string; value: number }) => {
-          return { type: x.trait_type, value: x.value };
-        }
-      ) as Trait[],
-    };
-    NFTs.push(nftRow as NFT);
+    const req = http.get(rawNFT.uri as string, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", async () => {
+        const metadata = JSON.parse(data);
+        const mint = rawNFT.mintAddress.toBase58();
+        const supply = await _solanaConnection.getTokenSupply(
+          new PublicKey(mint)
+        );
+        const nftRow = {
+          token: rawNFT.address.toBase58() as string,
+          mint: mint as string,
+          symbol: rawNFT.symbol as string,
+          supply: supply.value.uiAmount as number,
+          royalty: rawNFT.sellerFeeBasisPoints as number, // base 100 ex: 1000 = 10%
+          creators: rawNFT.creators.map(
+            (x: { address: string; share: number }) => {
+              return { pubkey: x.address, share: x.share };
+            }
+          ) as Creator[],
+          name: metadata.name as string,
+          description: metadata.description as string,
+          asset_uri: metadata.image as string,
+          metadata: metadata.attributes.map(
+            (x: { trait_type: string; value: number }) => {
+              return { type: x.trait_type, value: x.value };
+            }
+          ) as Trait[],
+        };
+        NFTs.push(nftRow as NFT);
+      });
+    });
+    req.on("error", (error) => {
+      console.error(error);
+    });
   }
   return NFTs;
 };
