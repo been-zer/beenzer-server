b09fa4c6bbaae07561a047b491ebe864ddbedbbd
quantium <alexcollsoutumuro@gmail.com>
2023-01-19 12:36:26 +0100

    update sendNFT socket

--- v2/src/services/sendNFT.ts
+++ v2/src/services/sendNFT.ts
@@ -1,30 +1,48 @@
-import { getOrCreateAssociatedTokenAccount, createTransferInstruction } from "@solana/spl-token";
-import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, Transaction } from "@solana/web3.js";
-import { Socket } from 'socket.io';
-import dotenv from 'dotenv';
+import {
+  getOrCreateAssociatedTokenAccount,
+  createTransferInstruction,
+} from "@solana/spl-token";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  sendAndConfirmTransaction,
+  Transaction,
+} from "@solana/web3.js";
+import { Socket } from "socket.io";
+import dotenv from "dotenv";
 import { sleep } from "../utils";
 dotenv.config();
 
-const secret = String(process.env.MASTER_WALLET_KEYPAIR).split(',') as any;
+const secret = String(process.env.MASTER_WALLET_KEYPAIR).split(",") as any;
 const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL as string;
 const SOLANA_CONNECTION = new Connection(SOLANA_RPC_URL);
 const WALLET = Keypair.fromSecretKey(new Uint8Array(secret));
 
-export async function sendNFT( socket: Socket, destination: string, token: PublicKey, supply: number ) {
-​
-  socket.emit('mintLogs', `Sending ${supply} ${(token.toString())} from ${(WALLET.publicKey.toString())} to ${(destination)}.`);
-  console.log(`Sending ${supply} ${(token.toString())} from ${(WALLET.publicKey.toString())} to ${(destination)}.`)
-  
+export async function sendNFT(
+  socket: Socket,
+  destination: string,
+  token: string,
+  supply: number
+) {
+  socket.emit(
+    "mintLogs",
+    `Sending ${supply} ${token.toString()} from ${WALLET.publicKey.toString()} to ${destination}.`
+  );
+  console.log(
+    `Sending ${supply} ${token.toString()} from ${WALLET.publicKey.toString()} to ${destination}.`
+  );
+
   //Step 1
   console.log(`1 - Getting Source Token Account`);
   let sourceAccount: any;
   let i = 0;
-  while ( i < 10 ) {
+  while (i < 10) {
     try {
       sourceAccount = await getOrCreateAssociatedTokenAccount(
-        SOLANA_CONNECTION, 
+        SOLANA_CONNECTION,
         WALLET,
-        token,
+        new PublicKey(token),
         WALLET.publicKey
       );
       console.log(`    Source Account: ${sourceAccount.address.toString()}`);
@@ -40,15 +58,17 @@ export async function sendNFT( socket: Socket, destination: string, token: Publi
   console.log(`2 - Getting Destination Token Account`);
   let destinationAccount: any;
   let j = 0;
-  while ( j < 10 ) {
+  while (j < 10) {
     try {
       destinationAccount = await getOrCreateAssociatedTokenAccount(
-      SOLANA_CONNECTION, 
-      WALLET,
-      new PublicKey(token),
-      new PublicKey(destination)
+        SOLANA_CONNECTION,
+        WALLET,
+        new PublicKey(token),
+        new PublicKey(destination)
       );
-      console.log(`    Destination Account: ${destinationAccount.address.toString()}`);
+      console.log(
+        `    Destination Account: ${destinationAccount.address.toString()}`
+      );
       break;
     } catch (error) {
       console.log(error);
@@ -56,33 +76,40 @@ export async function sendNFT( socket: Socket, destination: string, token: Publi
       j++;
     }
   }
-​
   //Step 3
   console.log(`3 - Fetching Number of Decimals for Mint: ${token}`);
-  const numberDecimals = 0;// await getNumberDecimals(MINT_ADDRESS);
+  const numberDecimals = 0; // await getNumberDecimals(MINT_ADDRESS);
   // console.log(`    Number of Decimals: ${numberDecimals}`);
-​
   //Step 4
   console.log(`4 - Creating and Sending Transaction`);
-  
+
   let k = 0;
-  while ( k < 10 ) {
+  while (k < 10) {
     try {
       const tx = new Transaction();
-      tx.add(createTransferInstruction(
+      tx.add(
+        createTransferInstruction(
           sourceAccount.address,
           destinationAccount.address,
           WALLET.publicKey,
           supply * Math.pow(10, numberDecimals)
-      ))
-      const latestBlockHash = await SOLANA_CONNECTION.getLatestBlockhash('confirmed');
-      tx.recentBlockhash = latestBlockHash.blockhash;    
-      const signature = await sendAndConfirmTransaction(SOLANA_CONNECTION,tx,[WALLET]);
-      socket.emit('mintLogs', `Transaction Success! 🎉 \n https://explorer.solana.com/tx/${signature}?cluster=mainnet-beta`);
+        )
+      );
+      const latestBlockHash = await SOLANA_CONNECTION.getLatestBlockhash(
+        "confirmed"
+      );
+      tx.recentBlockhash = latestBlockHash.blockhash;
+      const signature = await sendAndConfirmTransaction(SOLANA_CONNECTION, tx, [
+        WALLET,
+      ]);
+      socket.emit(
+        "mintLogs",
+        `Transaction Success! 🎉 \n https://explorer.solana.com/tx/${signature}?cluster=mainnet-beta`
+      );
       console.log(
-          '\x1b[32m', //Green Text
-          `   Transaction Success! 🎉`,
-          `\n    https://explorer.solana.com/tx/${signature}?cluster=mainnet-beta`
+        "\x1b[32m", //Green Text
+        `   Transaction Success! 🎉`,
+        `\n    https://explorer.solana.com/tx/${signature}?cluster=mainnet-beta`
       );
       k = 10;
       break;
@@ -94,5 +121,4 @@ export async function sendNFT( socket: Socket, destination: string, token: Publi
   }
 
   return true;
-  
 }
