d86cd49686cd8c0cf80ca8daa3bbfcf4ebd77cd6
quantium <alexcollsoutumuro@gmail.com>
2023-01-19 16:22:57 +0100

    update nft schema

--- v2/src/controllers/nfts.schemas.ts
+++ v2/src/controllers/nfts.schemas.ts
@@ -1,6 +1,6 @@
 // Tables initialization with column types
 export const nftInit =
-  "_id_ BIGINT NOT NULL UNIQUE, __token__ VARCHAR(255) PRIMARY KEY, _supply INT, _creator VARCHAR(255), _username VARCHAR(255), _asset VARCHAR(255), _type VARCHAR(25), _name VARCHAR(255), _description VARCHAR(2048), _city VARCHAR(75), _latitude DOUBLE PRECISION, _longitude DOUBLE PRECISION,  _distance VARCHAR(55), _minLat DOUBLE PRECISION, _maxLat DOUBLE PRECISION, _minLon DOUBLE PRECISION, _maxLon DOUBLE PRECISION, _date VARCHAR(22), _time VARCHAR(22), _timestamp BIGINT";
+  "_id_ BIGINT NOT NULL UNIQUE, __token__ VARCHAR(255) PRIMARY KEY, _supply INT, _creator VARCHAR(255), _username VARCHAR(255), _asset VARCHAR(255), _type VARCHAR(25), _name VARCHAR(255), _description VARCHAR(2048), _city VARCHAR(75), _latitude DOUBLE PRECISION, _longitude DOUBLE PRECISION,  _distance VARCHAR(55), _maxLat DOUBLE PRECISION, _minLat DOUBLE PRECISION, _maxLon DOUBLE PRECISION, _minLon DOUBLE PRECISION, _date VARCHAR(22), _time VARCHAR(22), _timestamp BIGINT";
 export const counterInit = "_n BIGINT, _timestamp BIGINT";
 export const ownersInit =
   "_token VARCHAR(255), _owner VARCHAR(255), _timestamp BIGINT";
