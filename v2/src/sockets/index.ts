9f77e77f46267f0fca8119578d17207e29110385
quantium <alexcollsoutumuro@gmail.com>
2023-01-19 15:01:38 +0100

    update NFTsockeeeeet2

--- v2/src/sockets/index.ts
+++ v2/src/sockets/index.ts
@@ -1,8 +1,8 @@
-import { Server, Socket } from 'socket.io';
-import userSocket from './users.socket';
-import messagesSocket from './messages.socket';
-import nftsSocket from './nfts.socket';
-import tokenSocket from './token.socket';
+import { Server, Socket } from "socket.io";
+import userSocket from "./users.socket";
+import messagesSocket from "./messages.socket";
+import nftsSocket from "./nfts.socket";
+import tokenSocket from "./token.socket";
 
 export let usersConnected: number = 0;
 
@@ -10,19 +10,19 @@ export const socketConnect = (io: Server): void => {
   io.on("connection", (socket: Socket) => {
     // Connection
     usersConnected++;
-    socket.emit('nUsers', usersConnected)
-    console.log(usersConnected, 'users connected.');
+    socket.emit("nUsers", usersConnected);
+    console.log(usersConnected, "users connected.");
     socket.emit("serverConnection", "Client connected to server succesfully");
     // Disconnection
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       usersConnected--;
-      console.log(usersConnected, 'users connected.')
-      socket.emit('nUsers', usersConnected)
+      console.log(usersConnected, "users connected.");
+      socket.emit("nUsers", usersConnected);
     });
     // Sockets
     userSocket(socket);
     messagesSocket(socket);
     nftsSocket(socket);
-    tokenSocket(socket);  
+    tokenSocket(socket);
   });
 };
