afe57dc3b4d9e8a748882758f0e3ef5f5f7ca420
quantium <alexcollsoutumuro@gmail.com>
2023-02-16 15:17:40 +0100

    finish getUserFeed.ts v1

--- v2/src/services/nfts/getFeedByLocation.ts
+++ v2/src/services/nfts/getFeedByLocation.ts
@@ -4,22 +4,29 @@ import { getUserFollows } from "../../controllers/users.controller";
 const getFeedbyLocation = async (
   pubkey: string,
   latitude: number,
-  longitude: number
+  longitude: number,
+  _logs: boolean = true // Optional, print logs
 ): Promise<any> => {
   try {
     const follows = await getUserFollows(pubkey);
     const following = [];
-    for (const user of follows.rows) {
+    for (const user of follows) {
       following.push(user.__pubkey__);
     }
     const mapNFTs = await getMapNFTs(latitude, longitude);
     const userFeed = [];
-    for (const nft of mapNFTs.rows) {
-      for (const follow in following) {
+    for (const nft of mapNFTs) {
+      for (const follow of following) {
         if (nft._creator == follow) {
           userFeed.push(nft);
         }
       }
+    }
+    if (_logs) {
+      console.log("\nfollows\n", follows);
+      console.log("\nfollowing\n", following);
+      console.log("\nmapNFTs\n", mapNFTs);
+      console.log("\nuserFeed\n", userFeed);
     }
     return userFeed;
   } catch (error) {
