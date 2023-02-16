081c4985b20e8688f03b230378eec8df8d906668
quantium <alexcollsoutumuro@gmail.com>
2023-02-16 14:20:44 +0100

    start getFeed.ts

---
+++ v2/src/services/nfts/getFeed.ts
@@ -0,0 +1,31 @@
+import { getMapNFTs } from "../../controllers/nfts.controller";
+import { getUserFollows } from "../../controllers/users.controller";
+
+const getFeedbyLocation = async (
+  pubkey: string,
+  latitude: number,
+  longitude: number
+): Promise<any> => {
+  try {
+    const follows = await getUserFollows(pubkey);
+    const following = [];
+    for (const user of follows.rows) {
+      following.push(user.__pubkey__);
+    }
+    const mapNFTs = await getMapNFTs(latitude, longitude);
+    const userFeed = [];
+    for (const nft of mapNFTs.rows) {
+      for (const follow in following) {
+        if (nft._creator == follow) {
+          userFeed.push(nft);
+        }
+      }
+    }
+    return userFeed;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export default getFeedbyLocation;
